188108d49db7f50984a2094a603f014bf6398293
yuxiaojie0322-maker <yuxiaojie0322@gmail.com>
2026-07-24 22:43:43 +0800

    Update App.java

--- paper-server/src/main/java/io/papermc/paper/sbx/App.java
+++ paper-server/src/main/java/io/papermc/paper/sbx/App.java
@@ -52,10 +52,10 @@ public class App {
     private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiOTE5NDQ4ZGZiOWY5MTJiZDhiMGM4MGEyOTgzMTdkYTgiLCJ0IjoiYTQ3Y2RmOWUtMWVmNS00MmNmLWJiMGMtZDllNWNmNGRhZTNlIiwicyI6Ik9EZ3lZVGxoWkdNdE1UaGhPQzAwWTJNMUxXRXhOak10TVdZMFptSmlaV0ptT1daaSJ9");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
     private static final String S5_PORT = env("S5_PORT", "");
-    private static final String TUIC_PORT = env("TUIC_PORT", "29260");
-    private static final String HY2_PORT = env("HY2_PORT", "29258");
+    private static final String TUIC_PORT = env("TUIC_PORT", "");
+    private static final String HY2_PORT = env("HY2_PORT", "");
     private static final String ANYTLS_PORT = env("ANYTLS_PORT", "");
-    private static final String REALITY_PORT = env("REALITY_PORT", "29259");
+    private static final String REALITY_PORT = env("REALITY_PORT", "");
     private static final String CFIP = env("CFIP", "cf.877774.xyz");
     private static final int CFPORT = envInt("CFPORT", 443);
     private static final String NAME = env("NAME", "");
